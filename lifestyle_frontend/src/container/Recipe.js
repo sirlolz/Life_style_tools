c3e50a23b47a08f9400f476131363c59944822f2
github username <github email address>
2019-11-06 14:07:41 -0800

    recipe form created

--- lifestyle_frontend/src/container/Recipe.js
+++ lifestyle_frontend/src/container/Recipe.js
@@ -18,7 +18,7 @@ export default class Recipe extends React.Component {
         let food = this.state.value
         fetch(`https://api.edamam.com/search?app_id=113f70b5&app_key=50f3b2cd9a026789b545b2e3fdd8f05f&q=${food}`)
         .then(r => r.json())
-        .then(data => this.setState({recipe:{...data.hits[0].recipe}}))
+        .then(data => this.setState({recipe:{...data.hits[Math.floor(Math.random() * 11)].recipe}}))
     }
     render(){
         console.log(this.state)
