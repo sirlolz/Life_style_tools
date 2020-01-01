a76c450a5e91898cafec5ab42b14829818fb168a
github username <github email address>
2019-12-31 16:37:22 -0800

    Updates

---
+++ precache-manifest.e2bb01936dc072e3fad7cab361917f3c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae6a4f3eaf0f7e32f8003ad064ebe206",
+    "url": "/Life_style_tools/index.html"
+  },
+  {
+    "revision": "5352ad762470f54169f5",
+    "url": "/Life_style_tools/static/css/main.0c6dfc41.chunk.css"
+  },
+  {
+    "revision": "7858bb6e52528f0bab9d",
+    "url": "/Life_style_tools/static/js/2.f80d15ca.chunk.js"
+  },
+  {
+    "revision": "5352ad762470f54169f5",
+    "url": "/Life_style_tools/static/js/main.1a9c2f58.chunk.js"
+  },
+  {
+    "revision": "c3c274b6d26cb1ca0161",
+    "url": "/Life_style_tools/static/js/runtime-main.f4d36f43.js"
+  }
+]);
